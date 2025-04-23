632bbcadcf466ca3756cec1d6328036c14d604ea
shawnwwu <wslsdust@163.com>
2025-04-23 15:37:18 +0800

    add initial files

---
+++ script.js
@@ -0,0 +1,139 @@
+$('.start').click(function(){
+    $('.stage1').fadeOut();
+    fire_modal('https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/cake_modal.png','开始做蛋糕啦!','今天是我宝儿的生日，我觉得让你自己制作一个数字生日蛋糕挺不错的~😄。首先咱们先制作蛋糕面糊🫓，然后把它放进数字烤箱里烤制🥯，最后给蛋糕各层来点装饰🎂。希望宝子玩得开心😄，生日快乐哦🥳🥳🥳！');
+})
+
+progress = 1;
+
+$('.modal_content button').click(function(){
+    progress++;
+    close_modal(progress)
+})
+
+function close_modal(callback){
+    modal.css('transform','translateY(-50%) scale(0)')
+    setTimeout(function(){
+        $('.stage' + callback).fadeIn();
+    },600)
+}
+
+
+function fire_modal(imgurl,title,content){
+
+    modal = $('.birthday_inner__modal');
+    modal.find('h1').html(title);
+    modal.find('img').attr('src',imgurl);
+    modal.find('p').html(content);
+    setTimeout(function(){
+        modal.css('transform','translateY(-50%) scale(1)')
+    },1000)
+
+
+}
+
+
+mixing = false;
+mixtimes = 0;
+
+$('.mixer').click(function(){
+    if(mixing == false){
+        mixing = true
+        mixtimes++;
+        $('.mix_spoon img').addClass('move')
+        setTimeout(function(){
+            $('.mix_spoon img').removeClass('move')
+            mixing = false;
+        },1000)
+    }
+    if(mixtimes == 6){
+        $('.stage2').fadeOut();
+        fire_modal('https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/mix_modal.png','搅拌成功！','我宝儿真棒！将混合物倒入烤盘🥞后，现在是时候把它放入我们的数字烤箱中了。经过一定时间我们会制作出一个漂亮的海绵状底部哦😄');
+
+    }
+
+})
+
+$('.tin').draggable({
+    revert:true
+})
+$( ".oven" ).droppable({
+    drop: function( event, ui ) {
+        $('.stage3').fadeOut();
+        fire_modal('https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/oven_modal.png','烘焙成功!','我宝儿真是一位大厨。底座已完全烘焙，看起来超级美味。现在是时候将这个底座与许多其他食材（如果酱🍓、巧克力🍫等）结合起来了🥳🥳🥳');
+    }
+})
+
+bases = 0;
+fillings = 0;
+
+$('.sponges .item_inner').click(function(){
+    $('.sponges').addClass('inactive')
+    $('.fillings').removeClass('inactive')
+    t = $(this).attr('class').split(' ').pop();
+    bases++
+    if(bases < 6){
+        add_sponge(t)
+    }
+})
+
+$('.fillings .item_inner').click(function(){
+    $('.fillings').addClass('inactive')
+    $('.sponges').removeClass('inactive')
+    f = $(this).attr('class').split(' ').pop();
+    fillings++
+    if(fillings < 7){
+        add_filling(f)
+    }
+})
+
+function add_sponge(t){
+
+    $('.cakemake').prepend('<div style="width:' + (200 - (bases * 20)) + 'px" class="sponge sponge-' + t + '"><div></div><div></div><div></div><div></div><div></div></div>')
+    $('.sponges h5 span').html(bases)
+}
+
+$('.startagain').click(function(){
+    $('.cakemake').html('<div class="base"></div>');
+    bases = 0;
+    fillings = 0;
+    $('.sponges h5 span').html(bases)
+    $('.fillings h5 span').html(fillings)
+     $('.fillings').removeClass('inactive')
+    $('.sponges').addClass('inactive')
+})
+function add_filling(f){
+
+    $('.cakemake').prepend('<div style="width:' + (200 - (bases * 20)) + 'px" class="filling filling-' + f + '"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>')
+    $('.fillings h5 span').html(fillings)
+}
+
+function fin(){
+    $('h1,h2,.options,.startagain,.add').fadeOut();
+    
+    setTimeout(function(){
+        $('.cakemake').fadeIn()
+        $('.cakemake').animate({'margin-top':'0px'})
+    },1000)
+    add_candle()
+    $('svg').addClass('text')
+}
+
+function add_candle(){
+    var stages = $('.cakemake > div').length;
+    var h = (stages/2) * 41 + 22 + 'px';
+    console.log(stages)
+    $('.cakemake').prepend('<div class="candle" ><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/candle.png" /></div>')
+    $('svg').show()
+    setTimeout(function(){
+        $('.sa').fadeIn()
+    },2200)
+    
+}
+
+$('.add').click(function(){
+    fin();
+})
+
+$('.sa').click(function(){
+    window.location.reload();
+})
